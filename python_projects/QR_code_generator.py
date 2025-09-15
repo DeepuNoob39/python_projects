82ed073d51fa4a167a4b63c9ccd619381bde6504
DeepuNoob39 <dhakadeepanshu30@gmail.com>
2025-09-16 01:13:31 +0530

    Update: multiple OR code generate

--- python_projects/QR_code_generator.py
+++ python_projects/QR_code_generator.py
@@ -1,15 +1,36 @@
 import qrcode
 
-data = input("Enter the text or URL: ").strip()
-filename = input("Enter the filename: ").strip()
+data = input("Enter the lists of text or URL using commas(,) : ").split(",")
+new_data = []
+for d in data:
+    d = d.strip()
+    new_data.append(d)
+    
+    
+# filename = input("Enter the filename: ").split(",")
+
+    
+# filler_color = input("Enter the filler color of the QR code: ")
+# background_color = input("Enter the background color of the QR code: ")
 
 qr = qrcode.QRCode(
     box_size=10,
     border=4
 )
-qr.add_data(data)
+for i, data in enumerate(new_data, start=1):
+    filename = f"qr_code{i}.png"
+    
+    qr = qrcode.QRCode(box_size=10, border=4)
+    qr.add_data(data)
+    
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(filename)
+    
+    print(f"QR for '{data}' saved as {filename}")
+    
+# qr.add_data(data)
 
-img = qr.make_image(fill_color="black", back_color="white")
-img.save(filename)
+# img = qr.make_image(fill_color= filler_color, back_color = background_color)
+# img.save(filename)
 
-print(f"QR is saved as {filename}")
+# print(f"QR is saved as {filename}")
