bcd4f2384546172f55b441dc4391cecaf80bddaa
DeepuNoob39 <dhakadeepanshu30@gmail.com>
2025-09-15 19:54:22 +0530

    fix: prevent crash when user enters non-numeric input

--- python_projects/guesser_game.py
+++ python_projects/guesser_game.py
@@ -6,7 +6,11 @@ secret_num = random.randint(1, range_of_secret_num)
 count = 0
 
 while True:
-    user_guess = int(input(f"Guess the number between 1 and {range_of_secret_num} : "))
+    try:
+        user_guess = int(input(f"Guess the number between 1 and {range_of_secret_num} : "))
+    except ValueError:
+        print("Please enter the valid number.")
+        continue
     if user_guess < secret_num:
         print("Your guess was too low, try again.")
         count += 1
