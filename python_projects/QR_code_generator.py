2871545cf2d19b14213d9ef59c48425ab97e400a
DeepuNoob39 <dhakadeepanshu30@gmail.com>
2025-09-15 23:50:49 +0530

    Add: QR code generator

---
+++ python_projects/QR_code_generator.py
@@ -0,0 +1,15 @@
+import qrcode
+
+data = input("Enter the text or URL: ").strip()
+filename = input("Enter the filename: ").strip()
+
+qr = qrcode.QRCode(
+    box_size=10,
+    border=4
+)
+qr.add_data(data)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save(filename)
+
+print(f"QR is saved as {filename}")
